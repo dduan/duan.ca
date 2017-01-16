f6f03985b0660d299e0f763fa604f571e08e0e0c
Daniel Duan <daniel@duan.org>
2017-01-16 14:41:07 -0800

    add permalink

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,1 +1,21 @@
 require 'jekyll/tagging'
+
+module Jekyll
+  module LinkBackFilter
+    def link_back(input, base, url)
+        total = input.scan(",").count
+        n = Random.rand(total)
+        count = 0
+        input.gsub(",") do |x|
+            dot = x
+            if n == count
+                dot = "<a href=#{base}#{url}>.</a>"
+            end
+            count += 1
+            dot
+        end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::LinkBackFilter)

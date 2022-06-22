1cae5ef41d2914e027aaa8e38841c48bfe573390
Daniel Duan <daniel@duan.ca>
2022-06-22 11:56:08 -0700

    fixup! [generator] Bump dependencies

--- generator/flake.nix
+++ generator/flake.nix
@@ -10,21 +10,25 @@
     flake-utils.lib.eachDefaultSystem (system:
       with nixpkgs.legacyPackages.${system};
       rec {
-        packages = flake-utils.lib.flattenTree {
-          generator = rustPlatform.buildRustPackage rec {
-            pname = "duan.ca-generator";
-            version = "0.1.0";
-            src = ./.;
-            cargoSha256 = "sha256-7aKeV7n4c9X03Fp9rL4CAdZMb0PDUUwZuXmiCWlprK0=";
-            lockFile = ./Cargo.lock;
+        packages = flake-utils.lib.flattenTree
+          {
+            generator = rustPlatform.buildRustPackage rec {
+              pname = "duan.ca-generator";
+              version = "0.1.0";
+              src = ./.;
+              cargoSha256 = "sha256-dIbynlmQ+wx1UzcyMv/yQfwkzQwfu/bovP55t+CIYKk=";
+              lockFile = ./Cargo.lock;
+              LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+            };
           };
-        };
         defaultPackage = packages.generator;
         devShell = pkgs.mkShell {
+          LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
           buildInputs = [
             cargo
             rust-analyzer
             rustc
+            libclang
           ];
         };
       });
